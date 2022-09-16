b6cd464542f0320a3c6101518d02aaeed3fba8bb
chencc33 <chencc3333@gmail.com>
2022-09-16 16:57:41 -0700

    initial setup for task

--- app/api/task_routes.py
+++ app/api/task_routes.py
@@ -1,1 +1,10 @@
 ## Get all tasks of current user
+from ..models.models import db, Workspace, Project, Task
+from ..models import db
+from flask import Blueprint, redirect
+
+task_routes = Blueprint('task', __name__, url_prefix='/api/tasks')
+
+@task_routes.route('/')
+def get_all_tasks():
+    return 'test'
