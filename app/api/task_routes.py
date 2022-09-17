dca9f9a480c726f20d366a93911fb0b78e2b4113
chencc33 <chencc3333@gmail.com>
2022-09-17 11:12:41 -0700

    get details of a task by id finished

--- app/api/task_routes.py
+++ app/api/task_routes.py
@@ -5,8 +5,16 @@ from flask import Blueprint, redirect
 
 task_routes = Blueprint('task', __name__, url_prefix='/api/tasks')
 
+## get all tasks of the current user
+## not finished yet, need user id
 @task_routes.route('')
 def get_all_tasks():
     tasks = Task.query.all()
-    response = [task.to_dict() for task in tasks]
-    return {"tasks": response}
+    response = {task.id: task.to_dict() for task in tasks}
+    return response
+
+#get details of a task by id
+@task_routes.route('/<int:taskId>')
+def get_task_by_id(taskId):
+    tasks = Task.query.get(taskId)
+    return tasks.to_dict()
