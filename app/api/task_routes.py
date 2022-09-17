edfbd627f5f3325c0712910577eb8133b79ecf4e
chencc33 <chencc3333@gmail.com>
2022-09-17 13:31:40 -0700

    get tasks by workspace and userid finished

--- app/api/task_routes.py
+++ app/api/task_routes.py
@@ -24,9 +24,20 @@ def get_task_by_id(taskId):
     return tasks.to_dict()
 
 #get all tasks by a project id
-#need to move this route to the project-routes
+#need to move this route to the project-routes and change the url a little if needed
 @task_routes.route('/<int:projectId>/tasks')
 def get_task_by_projectId(projectId):
     tasks = Task.query.filter(Task.project_id==projectId)
     response = {task.id:task.to_dict() for task in tasks}
     return response
+
+#Get all tasks by a user's id who is in the same workspace as current user
+#need to move this route to the workspace-routes and change the url a little if needed
+@task_routes.route('/<int:workspaceId>/users/<int:userId>/tasks')
+def tasks_by_workspace_userId(workspaceId, userId):
+    user_id = current_user.id
+    projects = Project.query.filter(Project.workspace_id==workspaceId, Project.owner_id==user_id)
+    response = {project.id:project.to_dict() for project in projects}
+    tasks = Task.query.join(Project).filter(Project.workspace_id==workspaceId, Project.owner_id==user_id).all()
+    response1 = {task.id:task.to_dict() for task in tasks}
+    return response1
