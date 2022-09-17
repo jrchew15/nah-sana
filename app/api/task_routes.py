aa24db462bca2256417b86a75b73828b42b04eb7
chencc33 <chencc3333@gmail.com>
2022-09-17 12:58:12 -0700

    get all tasks of the current user done

--- app/api/task_routes.py
+++ app/api/task_routes.py
@@ -1,7 +1,11 @@
 ## Get all tasks of current user
+from cgitb import reset
+from urllib import response
 from ..models.models import db, Workspace, Project, Task
+from ..models import User
 from ..models import db
 from flask import Blueprint, redirect
+from flask_login import current_user
 
 task_routes = Blueprint('task', __name__, url_prefix='/api/tasks')
 
@@ -9,8 +13,9 @@ task_routes = Blueprint('task', __name__, url_prefix='/api/tasks')
 ## not finished yet, need user id
 @task_routes.route('')
 def get_all_tasks():
-    tasks = Task.query.all()
-    response = {task.id: task.to_dict() for task in tasks}
+    user_id = current_user.id
+    tasks = Task.query.filter(Task.user_id==user_id)
+    response = {task.id:task.to_dict() for task in tasks}
     return response
 
 #get details of a task by id
