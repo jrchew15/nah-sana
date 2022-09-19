58284e00ee8c660868aa957f9288ec9f03195453
chencc33 <chencc3333@gmail.com>
2022-09-19 10:10:26 -0700

    create a task form

---
+++ react-app/src/components/Tasks/TaskForm.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { createOneTask } from '../../store/tasks';
+
+const SpotForm = () => {
+    useEffect(() => {
+
+    })
+}
