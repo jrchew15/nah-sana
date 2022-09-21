66a0f4157f28c4705fb1faca8f2dcdd223f7436b
Francisco Palacios <Fpalacios926@gmail.com>
2022-09-21 16:45:42 -0700

    finished create modal

--- react-app/src/components/Projects/CreateProjectModal/CreateProjectForm.js
+++ react-app/src/components/Projects/CreateProjectModal/CreateProjectForm.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom';
 import { createAProject } from '../../../store/projects';
+import './CreateProject.css'
 
-const CreateProjectForm = () => {
+const CreateProjectForm = ({ setShowModal }) => {
   const user = useSelector(state => state.session.user);
   const [errors, setErrors] = useState([]);
   const [name, setName] = useState('')
   const [status, setStatus] = useState('On Track')
   const [dueDate, setDueDate] = useState()
   const [description, setDescription] = useState('')
+  const [test, setClass] = useState('project-submit-button')
   let ownerId = user.id
   const { id } = useParams()
 
   const icon = `/static/images/icons/${Math.ceil(Math.random() * 5)}.png`
+  useEffect(() => {
+
+    if (name.length > 0) {
+      setClass('test')
+    }
+    if (name.length === 0) {
+      setClass('project-submit-button')
+    }
+  }, [name])
 
   let workspaceId = id
   const dispatch = useDispatch();
@@ -27,64 +38,76 @@ const CreateProjectForm = () => {
   };
 
   return (
-    <form onSubmit={createProject}>
-      {errors.length > 0 && (<div >
-        {errors.map((error, ind) => (
-          <div key={ind}>{error.split(":")[1]}</div>
-        ))}
-      </div>)}
-      <div>
-        <label>Name</label>
-        <input
-          type='text'
-          name='name'
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        ></input>
+    <div className='project-create-form-container'>
+      <div className='project-top-container'>
+        <h2>New Project</h2>
+        <button className="create-button" onClick={() => setShowModal(false)}>X</button>
       </div>
-      <div>
-        <label>Status</label>
-        <select
-          name='status'
-          onChange={(e) => setStatus(e.target.value)}
-          value={status}>
-          <option value="On Track">
-            On Track
-          </option>
-          <option value="At Risk">
-            At Risk
-          </option>
-          <option value="Off Track">
-            Off Track
-          </option>
-          <option value="On Hold">
-            On Hold
-          </option>
-          <option value="Complete">
-            Complete
-          </option>
-        </select>
-      </div>
-      <div>
-        <label>Due Date</label>
-        <input
-          type='date'
-          name='dueDate'
-          onChange={(e) => setDueDate(e.target.value)}
-          value={dueDate}
-        ></input>
-      </div>
-      <div>
-        <label>Description</label>
-        <textarea
-          type='text'
-          name='description'
-          onChange={(e) => setDescription(e.target.value)}
-          value={description}
-        ></textarea>
-      </div>
-      <button type='submit'>Submit</button>
-    </form>
+      <form onSubmit={createProject}>
+        {errors.length > 0 && (<div >
+          {errors.map((error, ind) => (
+            <div key={ind}>{error.split(":")[1]}</div>
+          ))}
+        </div>)}
+        {/* <div> */}
+        <div className='project-input-container'>
+          <label className='project-input-label'>Project Name</label>
+          <input
+
+            type='text'
+            name='name'
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          ></input>
+        </div>
+        <div className='project-input-container'>
+          <label className='project-input-label'>Status</label>
+          <select
+            className='project-select-class'
+            name='status'
+            onChange={(e) => setStatus(e.target.value)}
+            value={status}>
+            <option className='project-option' value="On Track">
+              On Track
+            </option>
+            <option value="At Risk">
+              At Risk
+            </option>
+            <option value="Off Track">
+              Off Track
+            </option>
+            <option value="On Hold">
+              On Hold
+            </option>
+            <option value="Complete">
+              Complete
+            </option>
+          </select>
+        </div>
+        <div className='project-input-container'>
+          <label className='project-input-label'>Due Date</label>
+          <input
+            type='date'
+            name='dueDate'
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          ></input>
+        </div>
+        <div className='project-input-container'>
+          <label className='project-input-label'>Description</label>
+          <textarea
+            className='text-area-style'
+            type='text'
+            name='description'
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          ></textarea>
+        </div>
+        <div className='project-input-container'>
+          <button className={`${test}`} type='submit'>Submit</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
